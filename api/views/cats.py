29e15b08b73cc573c9b507e571cd4dd69e72f238
Jayden Pak <jppak86@gmail.com>
2022-04-12 09:46:27 -0400

    finish back end

--- api/views/cats.py
+++ api/views/cats.py
@@ -3,8 +3,12 @@ from api.middleware import login_required, read_token
 
 from api.models.db import db
 from api.models.cat import Cat
+from api.models.feeding import Feeding
+from api.models.toy import Toy
+from api.models.toy import Association
 
 cats = Blueprint('cats', 'cats')
+
 
 @cats.route('/', methods=["POST"])
 @login_required
@@ -17,16 +21,24 @@ def create():
   db.session.commit()
   return jsonify(cat.serialize()), 201
 
+
 @cats.route('/', methods=["GET"])
 def index():
   cats = Cat.query.all()
   return jsonify([cat.serialize() for cat in cats]), 200
 
+
 @cats.route('/<id>', methods=["GET"])
 def show(id):
   cat = Cat.query.filter_by(id=id).first()
   cat_data = cat.serialize()
-  return jsonify(cat=cat_data), 200
+  cat_data["fed"] = cat.fed_for_today()
+
+  toys = Toy.query.filter(Toy.id.notin_([toy.id for toy in cat.toys])).all()
+  toys=[toy.serialize() for toy in toys]
+
+  return jsonify(cat=cat_data, available_toys=toys), 200
+
 
 @cats.route('/<id>', methods=["PUT"]) 
 @login_required
@@ -57,3 +69,49 @@ def delete(id):
   db.session.delete(cat)
   db.session.commit()
   return jsonify(message="Success"), 200
+
+
+@cats.route('/<id>/feedings', methods=["POST"]) 
+@login_required
+def add_feeding(id):
+  data = request.get_json()
+  data["cat_id"] = id
+
+  profile = read_token(request)
+  cat = Cat.query.filter_by(id=id).first()
+
+  if cat.profile_id != profile["id"]:
+    return 'Forbidden', 403
+
+  feeding = Feeding(**data)
+  
+  db.session.add(feeding)
+  db.session.commit()
+
+  cat_data = cat.serialize()
+  cat_data["fed"] = cat.fed_for_today()
+
+  return jsonify(cat_data), 201
+
+@cats.route('/<cat_id>/toys/<toy_id>', methods=["LINK"]) 
+@login_required
+def assoc_toy(cat_id, toy_id):
+  data = { "cat_id": cat_id, "toy_id": toy_id }
+
+  profile = read_token(request)
+  cat = Cat.query.filter_by(id=cat_id).first()
+  
+  if cat.profile_id != profile["id"]:
+    return 'Forbidden', 403
+
+  assoc = Association(**data)
+  db.session.add(assoc)
+  db.session.commit()
+
+  cat = Cat.query.filter_by(id=cat_id).first()
+  return jsonify(cat.serialize()), 201
+
+
+@cats.errorhandler(Exception)          
+def basic_error(err):
+  return jsonify(err=str(err)), 500
